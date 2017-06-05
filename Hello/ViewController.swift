6ad8dbb8adfa44cdb08979eebdd6be93c866c81a
ALEKSANDER ONISZCZAK <aleks@aleks-macbook-pro.local>
2017-06-05 11:24:37 -0400

    Initial Commit

---
+++ Hello/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by ALEKSANDER ONISZCZAK on 2017-06-05.
+//  Copyright © 2017 ALEKSANDER ONISZCZAK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
